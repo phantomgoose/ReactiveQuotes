6dcd55c597696f2084b1d07cb76ac0840860e370
Alaric von Teplitz <alaric2000@gmail.com>
2022-10-02 20:25:33 -0700

    Begin using the new React 18 concurrent renderer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
+import { createRoot } from 'react-dom/client';
 import Quotes from "./components/Quotes/Quotes";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<Quotes />, document.getElementById("root"));
-registerServiceWorker();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Quotes />);
